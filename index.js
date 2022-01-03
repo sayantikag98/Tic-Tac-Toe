e4a948f1abbb276113be6a0531d2283320636ef9
Sayantika Ghosh <sayantikaghosh98@gmail.com>
2022-01-03 11:29:14 +0530

    Added color to the winning formation

--- index.js
+++ index.js
@@ -9,15 +9,16 @@ playerTwoTurn.disabled = true;
 const result = document.querySelector(".result");
 let activePlayer = 1;
 
+
 // function for deciding the winner between player 1 and player 2
 const winnerDecidingFunction = () => {
     if(((boardTile[0].textContent !== "") && (boardTile[0].textContent === boardTile[1].textContent) && (boardTile[1].textContent === boardTile[2].textContent)) 
     || ((boardTile[3].textContent !== "") && (boardTile[3].textContent === boardTile[4].textContent) && (boardTile[4].textContent === boardTile[5].textContent)) 
     || ((boardTile[6].textContent !== "") && (boardTile[6].textContent === boardTile[7].textContent) && (boardTile[7].textContent === boardTile[8].textContent)) 
-    || ((boardTile[0].textContent !== "") &&(boardTile[0].textContent === boardTile[3].textContent) && (boardTile[3].textContent === boardTile[6].textContent)) 
-    || ((boardTile[1].textContent !== "") &&(boardTile[1].textContent === boardTile[4].textContent) && (boardTile[4].textContent === boardTile[7].textContent)) 
-    || ((boardTile[2].textContent !== "") &&(boardTile[2].textContent === boardTile[5].textContent) && (boardTile[5].textContent === boardTile[8].textContent)) 
-    || ((boardTile[0].textContent !== "") &&(boardTile[0].textContent === boardTile[4].textContent) && (boardTile[4].textContent === boardTile[8].textContent)) 
+    || ((boardTile[0].textContent !== "") && (boardTile[0].textContent === boardTile[3].textContent) && (boardTile[3].textContent === boardTile[6].textContent)) 
+    || ((boardTile[1].textContent !== "") && (boardTile[1].textContent === boardTile[4].textContent) && (boardTile[4].textContent === boardTile[7].textContent)) 
+    || ((boardTile[2].textContent !== "") && (boardTile[2].textContent === boardTile[5].textContent) && (boardTile[5].textContent === boardTile[8].textContent)) 
+    || ((boardTile[0].textContent !== "") && (boardTile[0].textContent === boardTile[4].textContent) && (boardTile[4].textContent === boardTile[8].textContent)) 
     || ((boardTile[2].textContent !== "") && (boardTile[2].textContent === boardTile[4].textContent) && (boardTile[4].textContent === boardTile[6].textContent)))
     {
         if(playerOneTurn.disabled === false){
@@ -40,6 +41,42 @@ const winnerDecidingFunction = () => {
     };
 };
 
+const helperFunc = (ind1, ind2, ind3) => {
+    boardTile[ind1].style.color = "red";
+    boardTile[ind2].style.color = "red";
+    boardTile[ind3].style.color = "red";
+    boardTile[ind1].style.backgroundColor = "yellow";
+    boardTile[ind2].style.backgroundColor = "yellow";
+    boardTile[ind3].style.backgroundColor = "yellow";
+}
+
+const colorWiningStreak = () => {
+    if((boardTile[0].textContent === boardTile[1].textContent) && (boardTile[1].textContent === boardTile[2].textContent)){
+        helperFunc(0, 1, 2);
+    }
+    else if((boardTile[0].textContent === boardTile[4].textContent) && (boardTile[4].textContent === boardTile[8].textContent)){
+        helperFunc(0, 4, 8);
+    }
+    else if((boardTile[0].textContent === boardTile[3].textContent) && (boardTile[3].textContent === boardTile[6].textContent)){
+        helperFunc(0, 3, 6);
+    }
+    else if((boardTile[2].textContent === boardTile[5].textContent) && (boardTile[5].textContent === boardTile[8].textContent)){
+        helperFunc(2, 5, 8);
+    }
+    else if((boardTile[6].textContent === boardTile[7].textContent) && (boardTile[7].textContent === boardTile[8].textContent)){
+        helperFunc(6, 7, 8);
+    }
+    else if((boardTile[2].textContent === boardTile[4].textContent) && (boardTile[4].textContent === boardTile[6].textContent)){
+        helperFunc(2, 4, 6);
+    }
+    else if((boardTile[1].textContent === boardTile[4].textContent) && (boardTile[4].textContent === boardTile[7].textContent)){
+        helperFunc(1, 4, 7);
+    }
+    else if((boardTile[3].textContent === boardTile[4].textContent) && (boardTile[4].textContent === boardTile[5].textContent)){
+        helperFunc(3, 4, 5);
+    }
+};
+
 // for placing the cross or the circles on the board tile
 boardTile.forEach(tile => {
     tile.addEventListener("click", () => {
@@ -53,6 +90,7 @@ boardTile.forEach(tile => {
             }
             else {
                 activePlayer = 0;
+                colorWiningStreak();
             }  
         }
         else if(activePlayer === 2 && tile.textContent === ""){
@@ -64,6 +102,7 @@ boardTile.forEach(tile => {
             }
             else {
                 activePlayer = 0;
+                colorWiningStreak();
             }
         }
     });
@@ -73,6 +112,8 @@ boardTile.forEach(tile => {
 resetBtn.addEventListener("click", () => {
     boardTile.forEach(tile => {
         tile.textContent = "";
+        tile.style.color = "#557C55";
+        tile.style.backgroundColor = "#F2FFE9";
     });
     playerOneTurn.disabled = false;
     playerTwoTurn.disabled = true;
